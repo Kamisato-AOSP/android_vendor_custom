2b6c47182d7f34657c47dc98e4aad93fb963edf1
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2026-01-09 06:41:54 +0100

    pixelOS->Kamisato-AOSP
    Change-Id: Ied5ccd27ae7da090c7c2db6a6c0504c44843b55a

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -67,7 +67,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        ZIPPATH=`ls -tr "$OUT"/PixelOS_*.zip | tail -1`
+        ZIPPATH=`ls -tr "$OUT"/Kamiato-AOSP_*.zip | tail -1`
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
             return 1
--- build/tasks/pixelos.mk
+++ build/tasks/pixelos.mk
@@ -16,7 +16,7 @@
 # -----------------------------------------------------------------
 # PixelOS OTA update package
 
-CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/PixelOS_$(CUSTOM_VERSION).zip
+CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/Kamisato-AOSP_$(CUSTOM_VERSION).zip
 
 MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
