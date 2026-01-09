a1eabeed774d91005660ccf80d964b2d0fe895f3
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2026-01-09 12:37:15 +0100

    bringup stuff
    Change-Id: Ibcc9e09329819f6e7a55912120a50849c097e47a

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -29,7 +29,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka pixelos
+        mka Kamisato-AOSP
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
